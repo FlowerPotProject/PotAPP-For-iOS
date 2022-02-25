979933ff27b467c1994b1ca6ee7a2bf541fd6704
seunggyun-jeong <jskyun98@gmail.com>
2022-02-25 17:42:19 +0900

    water supply page 2/3

--- HwaBun/HwaBun/control/waterControlViewController.swift
+++ HwaBun/HwaBun/control/waterControlViewController.swift
@@ -16,46 +16,178 @@ class waterControlViewController: UIViewController {
     @IBOutlet weak var soilHumidSlider: UISlider!
     @IBOutlet weak var soilHumidInput: UITextField!
     @IBOutlet weak var soilControlLabel: UILabel!
-    @IBOutlet weak var soilHumidConfirmButton: UIButton!
+    @IBOutlet weak var autoModeConfirmButton: UIButton!
+    
+    @IBOutlet weak var controlMethodSelectButton: UIButton!
+    @IBOutlet weak var selectMenu: UIMenu!
+    @IBOutlet weak var controlMethodSelectLabel: UILabel!
+    @IBOutlet weak var reserveDatePicker: UIDatePicker!
+    @IBOutlet weak var datePickerHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var supplyAmountSettingLabel: UILabel!
+    @IBOutlet weak var supplyMethodSelectButton: UIButton!
+    @IBOutlet weak var waterAmountInput: UITextField!
+    @IBOutlet weak var mLLabel: UILabel!
+    
+    @IBOutlet weak var manualModeConfirmButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateUI()
+        setControlMethodButton()
+        setSupplyMethodButton()
+        setDatePicker()
+        self.soilHumidInput.addTarget(self, action: #selector(textFieldChange(_:)), for: .editingChanged)
     }
     
+    // Auto Mode UI제어
     @IBAction func autoModeButtonAction(_ sender: Any) {
         autoModeButton.isSelected = !autoModeButton.isSelected
         let selected = autoModeButton.isSelected
         viewSize(view: autoModeSettingViewHeight, selected)
-        print("---> selected : \(selected)")
-    }
-    
-    @IBAction func manualModeButtonAction(_ sender: Any) {
-        manualModeButton.isSelected = !manualModeButton.isSelected
-        let selected = manualModeButton.isSelected
-        viewSize(view: manualModeSettingViewHeight, selected)
+        
         // 보이게 수정
         soilHumidSlider.isHidden = !selected
         soilHumidInput.isHidden = !selected
         soilControlLabel.isHidden = !selected
-        soilHumidConfirmButton.isHidden = !selected
+        autoModeConfirmButton.isHidden = !selected
+    }
+    
+    // Manual Mode UI제어
+    @IBAction func manualModeButtonAction(_ sender: Any) {
+        manualModeButton.isSelected = !manualModeButton.isSelected
+        let selected = manualModeButton.isSelected
+        viewSize(view: manualModeSettingViewHeight, selected)
+        controlMethodSelectButton.isHidden = !selected
+        controlMethodSelectLabel.isHidden = !selected
+        supplyAmountSettingLabel.isHidden = !selected
+        datePickerHideLogic(selected)
+        supplyMethodSelectButton.isHidden = !selected
+        manualModeConfirmButton.isHidden = !selected
+        waterAmountInput.isHidden = !selected
+        mLLabel.isHidden = !selected
+    }
+    
+    func datePickerHideLogic(_ selected: Bool) {
+        if selected == true {
+            guard let buttonTitle = controlMethodSelectButton.titleLabel?.text else { return }
+            if buttonTitle == "예약" {
+                reserveDatePicker.isHidden = false
+            }
+            else {
+                reserveDatePicker.isHidden = true
+            }
+        }
+        else {
+            reserveDatePicker.isHidden = true
+        }
     }
     
     @IBAction func controlSlider(_ sender: Any) {
         let value = soilHumidSlider.value
         soilHumidInput.text = "\(Int(value))"
-        
     }
     
+    @objc func textFieldChange(_ sender: Any) {
+        guard var convert = Float(self.soilHumidInput.text ?? "50") else {
+            return
+        }
+        if(convert >= 100) {
+            soilHumidInput.text = "100"
+            convert = 100
+        }
+        self.soilHumidSlider.setValue(convert, animated: true)
+    }
+    // 화면 탭 시 키보드 내려감
+    @IBAction func tapBG(_ sender: Any) {
+        soilHumidInput.resignFirstResponder()
+        waterAmountInput.resignFirstResponder()
+    }
+    
+    // 오토 모드 설정 확인
+    @IBAction func confirmAutoMode(_ sender: Any) {
+        print("오토 모드 설정 완료.")
+        print("습도 : \(soilHumidInput.text)%")
+    }
+    
+    // 메뉴얼 모드 설정 확인
+    @IBAction func manualModeConfirm(_ sender: Any) {
+        print("메뉴얼 모드 설정 완료.")
+        print("유량 : \(waterAmountInput.text)mL")
+    }
+    
+
+    
     func updateUI() {
+        // 자동 모드 UI 초기화
         autoModeSettingViewHeight.constant = 0
-        manualModeSettingViewHeight.constant = 0
         soilHumidSlider.isHidden = true
         soilHumidInput.isHidden = true
         soilControlLabel.isHidden = true
-        soilHumidConfirmButton.isHidden = true
+        autoModeConfirmButton.isHidden = true
+        
+        // 수동 모드 UI 초기화
+        manualModeSettingViewHeight.constant = 0
+        controlMethodSelectButton.isHidden = true
+        controlMethodSelectLabel.isHidden = true
+        supplyAmountSettingLabel.isHidden = true
+        reserveDatePicker.isHidden = true
+        supplyMethodSelectButton.isHidden = true
+        manualModeConfirmButton.isHidden = true
+        waterAmountInput.isHidden = true
+        mLLabel.isHidden = true
+        
+        // 텍스트 필드 숫자 키보드
+        soilHumidInput.keyboardType = .numberPad
+        waterAmountInput.keyboardType = .numberPad
+    }
+    
+    func setControlMethodButton() {
+        let direct = UIAction(title: "즉시", handler: { _ in
+            self.datePickerHeight.constant = 0
+            self.reserveDatePicker.isHidden = true
+        })
+        let reserve = UIAction(title: "예약", handler: { _ in
+            self.datePickerHeight.constant = 50
+            self.reserveDatePicker.isHidden = false
+        })
+
+        let buttonMenu = UIMenu(title: "제어 방식", children: [direct, reserve])
+        
+        controlMethodSelectButton.menu = buttonMenu
+    }
+    
+    func setSupplyMethodButton() {
+        let time = UIAction(title: "시간", handler: { _ in
+
+        })
+        
+        let amount = UIAction(title: "유량", handler: { _ in
+
+        })
+
+        let buttonMenu = UIMenu(title: "공급 유형", children: [time, amount])
+        
+        supplyMethodSelectButton.menu = buttonMenu
+    }
+    
+    private func setDatePicker() {
+        // 1년 뒤 까지 예약 가능
+        var maxDateComponents = DateComponents()
+        maxDateComponents.year = 1
+        reserveDatePicker.maximumDate = Calendar.autoupdatingCurrent.date(byAdding: maxDateComponents, to: Date())
+        
+        // 과거는 선택 불가
+        reserveDatePicker.minimumDate = Date()
+        reserveDatePicker.addTarget(self, action: #selector(datePicker(_ :)), for: .valueChanged)
+        datePickerHeight.constant = 0
+    }
+    
+    // DatePicker의 값 사용 (데이터 저장 필요)
+    @objc func datePicker(_ sender: UIDatePicker) {
+        print(sender.date)
     }
     
     private func viewSize(view: NSLayoutConstraint,_ show: Bool) {
@@ -66,7 +198,5 @@ class waterControlViewController: UIViewController {
             view.constant = 0
         }
     }
-    
-    
     
 }
