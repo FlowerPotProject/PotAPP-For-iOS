d9d47be63a03c76b99f948e91c3819b6e3519ee3
seunggyun-jeong <jskyun98@gmail.com>
2022-07-22 13:44:58 +0900

    [FEAT] 급수 시점 막대그래프로 표시

--- HwaBun/HwaBun/detail/GraphViewController.swift
+++ HwaBun/HwaBun/detail/GraphViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 import Charts
 
 class GraphViewController: UIViewController, ChartViewDelegate{
-    let chartDataTypeNames: [String] = ["토양습도", "대기습도", "대기온도"]
+    let chartDataTypeNames: [String] = ["토양습도 (%)", "대기습도 (%)", "대기온도 (℃)"]
     let chartPeriodicalTypeNames: [String] = ["일간", "주간", "월간"]
     
-    var lineChart = LineChartView()
-    
+    var lineChart = CombinedChartView()
     var soilHumi: [soilHumiData] = []
     var humi: [humiData] = []
     var temp: [tempData] = []
@@ -55,39 +54,61 @@ class GraphViewController: UIViewController, ChartViewDelegate{
         lineChart.borderColor = .black
         lineChart.center = view.center
         
-
-        
-        var entries = [ChartDataEntry]()
+        // Data Entry 설정
+        var lineEntries = [ChartDataEntry]()
+        var barEntries = [ChartDataEntry]()
         if type == .soilHumi {
+            var count = 0
+            
             for x in 0..<soilHumi.count {
-                entries.append(ChartDataEntry(x: Double(x), y: soilHumi[x].val))
-            }
-            // soilHumi/daily 일 때 물 주는 로그도 같이 띄움
-            if self.periodicalType == .daily {
+                lineEntries.append(ChartDataEntry(x: Double(x), y: soilHumi[x].val))
                 
+                // soilHumi/daily 일 때 물 주는 로그도 같이 띄움
+                if self.periodicalType == .daily,
+                   count < self.controlLog.count,
+                   soilHumi[x].time == controlLog[count]
+                {
+                    barEntries.append(BarChartDataEntry(x: Double(x), y: soilHumi[x].val))
+                    count += 1
+                }
             }
+            
         }
         else if type == .temp {
             for x in 0..<temp.count {
-                entries.append(ChartDataEntry(x: Double(x), y: temp[x].val))
+                lineEntries.append(ChartDataEntry(x: Double(x), y: temp[x].val))
             }
         }
         else {
             for x in 0..<humi.count {
-                entries.append(ChartDataEntry(x: Double(x), y: humi[x].val))
+                lineEntries.append(ChartDataEntry(x: Double(x), y: humi[x].val))
             }
             
         }
-
-        let set = LineChartDataSet(entries: entries)
-        set.label = chartDataTypeNames[type.rawValue]
-        set.colors = setChartColor(type: type)
-        set.highlightEnabled = false // 선택 안되게
-        set.drawCirclesEnabled = false // 동그라미 없애기
-        set.mode = .cubicBezier // 애니메이션 형태
-        lineChart.doubleTapToZoomEnabled = false // 줌 안되게
-        let data = LineChartData(dataSet: set)
+        
+        // Data Set 설정
+        let lineSet = LineChartDataSet(entries: lineEntries)
+        lineSet.label = chartDataTypeNames[type.rawValue]
+        lineSet.colors = setChartColor(type: type)
+        lineSet.highlightEnabled = false // 선택 안되게
+        lineSet.drawCirclesEnabled = false // 동그라미 없애기
+        lineSet.mode = .cubicBezier // 애니메이션 형태
+        
+        let barSet = BarChartDataSet(entries: barEntries)
+        barSet.label = "급수 시점"
+        barSet.colors = [.systemBlue]
+        barSet.highlightEnabled = false // 선택 안되게
+        let barData = BarChartData(dataSet: barSet)
+        barData.barWidth = 0.3
+        
+        // Chart Data 설정
+        let data = CombinedChartData()
+        data.lineData = LineChartData(dataSet: lineSet)
+        data.barData = barData
         data.setDrawValues(false)
+        
+        // Chart 속성 설정
+        lineChart.doubleTapToZoomEnabled = false // 줌 안되게
         lineChart.xAxis.labelPosition = .bottom // x축 레이블 위치 아래로
         lineChart.xAxis.valueFormatter = setIndexAxisValue(type: type) // x축 레이블 포메터 지정
         lineChart.rightAxis.enabled = false // 우측 레이블 제거
@@ -100,11 +121,11 @@ class GraphViewController: UIViewController, ChartViewDelegate{
     func setChartColor(type: dataType) -> [UIColor] {
         switch type {
         case .temp:
-            return [UIColor.red]
+            return [UIColor.systemPink]
         case .humi:
-            return [UIColor.blue]
-        case .soilHumi:
             return [UIColor.systemGreen]
+        case .soilHumi:
+            return [UIColor.systemBrown]
         }
     }
     
@@ -189,17 +210,19 @@ class GraphViewController: UIViewController, ChartViewDelegate{
     func loadData() {
         ServerAPI.loadGraghData(potId: 1, periodical: self.periodicalType.rawValue) { [self] GraphData in
             
+            self.controlLog = []
             self.soilHumi = GraphData.soilHumi
             self.temp = GraphData.temp
             self.humi = GraphData.humi
-            let controlLog: [log] = GraphData.log
             
+            let controlLog: [log] = GraphData.log
             for data in controlLog {
                 self.controlLog.append(data.time)
             }
             
-            var periodicalType: String
+            print(self.controlLog)
             
+            var periodicalType: String
             if self.periodicalType == .daily {
                 periodicalType = "일간"
             }
